fcc52ababd2070e81003a951fa589d3d89f68392
tjforeman <tylerforeman92@gmail.com>
2019-04-23 17:38:57 -0400

    get by id working

--- data/posts-router.js
+++ data/posts-router.js
@@ -31,4 +31,21 @@ router.post('/',(req,res)=>{
 }
 })
 
+router.get('/:id', (req,res)=>{
+    Db
+    .findById(req.params.id)
+    .then(post =>{
+        console.log(post)
+        if (post.length>0){
+            res.status(200).json(post)
+}else{
+    res.status(404).json({message: "The post with the specified ID does not exist."})
+}
+})
+    .catch(err =>{
+        res.status(500).json({error: "The post information could not be retrieved."})
+    })
+
+ })
+
 module.exports=router;
