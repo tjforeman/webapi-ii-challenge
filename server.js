fa9a2dcb4fffb763f2264eaf4af8626ae3f0cf2a
tjforeman <tylerforeman92@gmail.com>
2019-04-23 15:36:01 -0400

    server running

---
+++ server.js
@@ -0,0 +1,12 @@
+const express= require('express')
+
+const server= express();
+
+server.use(express.json());
+
+server.get('/', (req,res)=>{
+    res.send(`hello world`)
+})
+
+
+module.exports=server
